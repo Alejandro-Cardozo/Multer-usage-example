6b2f4e0bac494097269a09b12d810398065edea7
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-11-14 15:50:41 -0300

    Added nodemon and default view

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const path = require('path')
+//const ejs = require('ejs'); unnecessary
 
 //Initializations
 const app = express();
 
-// 
+//Settings
+app.set('port', 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Routes
+app.get('/',(req,res) => {
+	res.render('index');
+});
+
+// Start the server
+app.listen(app.get('port'), () => {
+	console.log(`Server on port ${app.get('port')}`);
+});
