9192362f05a8c70de4eef08d9d8966e1022565f6
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-11-14 16:15:56 -0300

    upload logic created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path')
+const multer = require('multer');
 //const ejs = require('ejs'); unnecessary
 
 //Initializations
@@ -10,9 +11,19 @@ app.set('port', 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Middlewares
+app.use(multer({
+	dest: path.join(__dirname, 'public/img')
+}).single('image'));
+
 //Routes
 app.get('/',(req,res) => {
 	res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+	console.log(req.file); //req.file has data from the uploaded file
+	res.send('uploaded');
 });
 
 // Start the server
